c11775d2e550ca6abd86320b2830fcaf3224adc0
Dokyun Lim <im.koremp@gmail.com>
2023-07-04 17:35:42 +0900

    add input component

---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { TextField } from '@mui/material';
+import { KeyboardEventHandler } from 'react';
+
+interface InputProps {
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onKeyDown?: KeyboardEventHandler<HTMLDivElement>;
+};
+
+export default function Input({
+  value,
+  onChange,
+  onKeyDown,
+}: InputProps) {
+  return (
+    <TextField
+      required
+      value={value}
+      onChange={onChange}
+      onKeyDown={onKeyDown}
+    />
+  );
+};
