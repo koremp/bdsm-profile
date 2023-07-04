186a4aaf12570d0326bb2537ee65528b8dbf0fa6
Dokyun Lim <im.koremp@gmail.com>
2023-07-04 17:26:49 +0900

    add single props and multiple props to props

--- src/types/Property.ts
+++
@@ -1,9 +0,0 @@
-export type SingleProperty = {
-  exist: boolean;
-  other: string;
-};
-
-export type MultipleProperty = {
-  exist: boolean;
-  others: string[];
-}
---
+++ src/types/Props.ts
@@ -0,0 +1,14 @@
+interface Props {
+  exist: boolean;
+  desc: string;
+};
+
+export interface SingleProps<T> extends Props {
+  value: T | undefined;
+  other: string;
+}
+
+export interface MultipleProps<T> extends Props {
+  values: T[];
+  others: string[];
+}
