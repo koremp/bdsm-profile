5940bff47ce193bf11f34dd8361bc272b665a805
Dokyun Lim <im.koremp@gmail.com>
2023-07-11 17:34:07 +0900

    add profile select index page

---
+++ pages/profile/index.tsx
@@ -0,0 +1,19 @@
+import {
+  Box,
+  Typography,
+} from '@mui/material';
+
+export default function ProfileSelectPage() {
+  return (
+    <>
+      <Typography>Profile Select Page</Typography>
+      <Box>
+        Gender
+        Romantic
+        Sexual
+        Relationship
+        BDSM
+      </Box>
+    </>
+  );
+};
