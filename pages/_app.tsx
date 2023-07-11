b2d2eda06c94d98ef33cd666fc63121c465000c4
Dokyun Lim <im.koremp@gmail.com>
2023-07-11 17:33:58 +0900

    add redux provider to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,16 +14,19 @@ import '@fontsource/roboto/700.css';
 
 import 'styles/global.css';
 
+import store from 'src/features/store';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionContextProvider supabaseClient={supabase}>
-      <Container maxWidth="xs" sx={{
-        backgroundColor: "#eeeeee",
-        height: "100vh",
-      }}>
-        <Component {...pageProps} />
-      </Container>
+      <Provider store={store}>
+        <Container maxWidth="xs" sx={{
+          backgroundColor: "#eeeeee",
+          height: "100vh",
+        }}>
+          <Component {...pageProps} />
+        </Container>
+      </Provider>
     </SessionContextProvider>
   )
 }
