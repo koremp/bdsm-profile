b490c07020d7a354e518a630371402d2049bb733
Dokyun Lim <im.koremp@gmail.com>
2023-07-22 19:53:21 +0900

    add romantic preference type

---
+++ src/types/RomanticPreference.ts
@@ -0,0 +1,51 @@
+import { SingleProps } from './Props';
+import { Information, convertInformation2Obj } from './Information';
+
+export const romanticPreferenceArray: readonly Information[] = [
+  {
+    text: 'Heteroromantic',
+    description: '',
+  },
+  {
+    text: 'Heteroflexibleromantic',
+    description: '',
+  },
+  {
+    text: 'Homoromantic',
+    description: '',
+  },
+  {
+    text: 'Homoflexibleromantic',
+    description: '',
+  },
+  {
+    text: 'Biromantic',
+    description: '',
+  },
+  {
+    text: 'Panromantic',
+    description: '',
+  },
+  {
+    text: 'Aromantic',
+    description: '',
+  },
+  {
+    text: 'Grayromantic',
+    description: '',
+  },
+  {
+    text: 'Demiromantic',
+    description: '',
+  },
+  {
+    text: 'Other',
+    description: '',
+  },
+];
+
+export type RomanticPreference = typeof romanticPreferenceArray[number]['text'];
+
+export const RomanticPreference = convertInformation2Obj(romanticPreferenceArray);
+
+export interface RomanticPrefenceProperty extends SingleProps<RomanticPreference> { };
