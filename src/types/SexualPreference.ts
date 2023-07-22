3442660c7c0cb5db3161d2eef0498ecb7926ea6b
Dokyun Lim <im.koremp@gmail.com>
2023-07-22 19:53:07 +0900

    add sexual preference type

---
+++ src/types/SexualPreference.ts
@@ -0,0 +1,59 @@
+import { SingleProps } from './Props';
+import { Information, convertInformation2Obj } from './Information';
+
+export const sexualPreferenceArray: readonly Information[] = [
+  {
+    text: 'Heterosexual',
+    description: '',
+  },
+  {
+    text: 'Heteroflexible',
+    description: '',
+  },
+  {
+    text: 'Homosexual',
+    description: '',
+  },
+  {
+    text: 'Homoflexible',
+    description: '',
+  },
+  {
+    text: 'Bisexual',
+    description: '',
+  },
+  {
+    text: 'Pansexual',
+    description: '',
+  },
+  {
+    text: 'Demisexual',
+    description: '',
+  },
+  {
+    text: 'Graysexual',
+    description: '',
+  },
+  {
+    text: 'Asexual',
+    description: '',
+  },
+  {
+    text: 'Androsexual',
+    description: '',
+  },
+  {
+    text: 'Gynesexual',
+    description: '',
+  },
+  {
+    text: 'Other',
+    description: '',
+  },
+];
+
+export type SexualPreference = typeof sexualPreferenceArray[number]['text'];
+
+export const SexualPreference = convertInformation2Obj(sexualPreferenceArray);
+
+export interface SexualPreferenceProperty extends SingleProps<SexualPreference> { };
