2a4901a62432aee18478c16f6fd49d3882001599
Dokyun Lim <im.koremp@gmail.com>
2023-07-22 19:53:47 +0900

    add relationship preference type

---
+++ src/types/RelationshipPreference.ts
@@ -0,0 +1,39 @@
+import { SingleProps } from './Props';
+import { Information, convertInformation2Obj } from './Information';
+
+export const relationshipPreferenceArray: readonly Information[] = [
+  {
+    text: 'Talk',
+    description: '',
+  },
+  {
+    text: 'One Night Stand',
+    description: '',
+  },
+  {
+    text: 'For BDSM(Play Partner)',
+    description: '',
+  },
+  {
+    text: 'Sex Partner',
+    description: '',
+  },
+  {
+    text: 'Friend with Benefit',
+    description: '',
+  },
+  {
+    text: 'Relationship',
+    description: '',
+  },
+  {
+    text: 'Other',
+    description: '',
+  },
+];
+
+export type RelationshipPreference = typeof relationshipPreferenceArray[number]['text'];
+
+export const RelationshipPreference = convertInformation2Obj(relationshipPreferenceArray);
+
+export interface RelationshipPrefenceProperty extends SingleProps<RelationshipPreference> { };
