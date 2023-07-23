b145f3c8ca4e8d727b1a8f051c840e7b849bd989
Dokyun Lim <im.koremp@gmail.com>
2023-07-23 18:51:55 +0900

    add bdsm codes to guide page

--- pages/guide/index.tsx
+++ pages/guide/index.tsx
@@ -1,9 +1,16 @@
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 
 export default function GuidePage() {
   return (
     <>
-      <Typography>BDSM Guide</Typography>
+      <Typography>BDSM Guide Page</Typography>
+      <Box>
+        <Typography>SSC</Typography>
+        <Typography>RACK</Typography>
+        <Typography>PRICK</Typography>
+        <Typography>CNC</Typography>
+        <Typography>CCCC</Typography>
+      </Box>
     </>
   );
 };
