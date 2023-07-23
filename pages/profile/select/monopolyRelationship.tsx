c7b2af26b0719f846cd50c97b3bd2002cbe42cf6
Dokyun Lim <im.koremp@gmail.com>
2023-07-23 18:47:15 +0900

    add monopoly relationship select page

---
+++ pages/profile/select/monopolyRelationship.tsx
@@ -0,0 +1,7 @@
+import MonopolyRelationshipComponent from 'src/components/select/MonopolyRelationship';
+
+export default function MonopolyRelationshipSelectProfilePage() {
+  return (
+    <MonopolyRelationshipComponent />
+  );
+};
