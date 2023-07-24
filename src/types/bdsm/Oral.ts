aa30275102c82058a141d0160cecf34276294893
Dokyun Lim <im.koremp@gmail.com>
2023-07-24 18:07:51 +0900

    add oral bdsm type

---
+++ src/types/bdsm/Oral.ts
@@ -0,0 +1,12 @@
+// 12
+
+export const Oral = {
+  Cunnilingus: 'Cunnilingus',
+  DeepThroat: 'Deep Throat',
+  Swallow: 'Swallow',
+  FaceEjaculation: 'Face Ejaculation',
+  FaceSitting: 'Face Sitting',
+  SixtyNine: 'Sixty Nine',
+};
+
+export type Oral = typeof Oral[keyof typeof Oral];
