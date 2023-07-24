394d56474ad417357f986fe993964ba53e9a0afd
Dokyun Lim <im.koremp@gmail.com>
2023-07-24 18:01:20 +0900

    add non penetration bdsm type

---
+++ src/types/bdsm/NonPenetration.ts
@@ -0,0 +1,12 @@
+// 10
+
+export const NonPenetration = {
+  Masturbation: 'Masturbation',
+  Footjob: 'Footjob',
+  Handjob: 'Handjob',
+  Fingering: 'Fingering',
+  MammaryIntercourse: 'Mammary Intercourse',
+  Humping: 'Humping',
+};
+
+export type NonPenetration = typeof NonPenetration[keyof typeof NonPenetration];
