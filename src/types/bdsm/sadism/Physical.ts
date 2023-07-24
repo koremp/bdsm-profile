40d6e1e984135f31f81d1b3be76e783415acdd7c
Dokyun Lim <im.koremp@gmail.com>
2023-07-24 17:26:02 +0900

    add physical bdsm sadism type

---
+++ src/types/bdsm/sadism/Physical.ts
@@ -0,0 +1,18 @@
+// 3
+
+export const Physical = {
+  PullHair: 'Pull Hair',
+  Press: 'Press',
+  Claw: 'Claw',
+  Bite: 'Bite',
+  BreathControl: 'Breath Control',
+  OrgasmControl: 'Orgasm Control',
+  MultiOrgasm: 'Multi Orgasm',
+  BallBusting: 'Ball Busting',
+  Needle: 'Needle',
+  Blood: 'Blood',
+  Torture: 'Torture',
+  Lynch: 'Lynch',
+};
+
+export type Physical = typeof Physical[keyof typeof Physical];
