63d946f1127ed83e8817ed8e920bf758f9b9d6ac
Dokyun Lim <im.koremp@gmail.com>
2023-07-24 17:56:01 +0900

    add multiplay bdsm type

---
+++ src/types/bdsm/Multyplay.ts
@@ -0,0 +1,12 @@
+// 9
+
+export const Multiplay = {
+  Watch: 'Watch',
+  Threesome: 'Threesome',
+  SimultaneousPenetration: 'Simultaneous Penetration',
+  Groupsex: 'Group Sex',
+  Promiscuity: 'Promiscuity',
+  Gangbang: 'Gangbang',
+};
+
+export type Multiplay = typeof Multiplay[keyof typeof Multiplay];
