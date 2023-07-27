53f3173483dfa8c816713248a0a252b4c88b3e3f
Dokyun Lim <im.koremp@gmail.com>
2023-07-27 19:32:10 +0900

    add link to profile select page

--- pages/profile/select/index.tsx
+++ pages/profile/select/index.tsx
@@ -1,35 +1,71 @@
-import { Box, Typography } from '@mui/material';
+import { Box, Button, Link, Typography } from '@mui/material';
 
-export default function BasicSelectProfilePage() {
+export default function IndexSelectProfilePage() {
   return (
     <>
-      <Typography>Basic Information</Typography>
-      <Box>
-        <Typography>Result</Typography>
-      </Box>
-      <Box>
-        <Typography>Age</Typography>
-      </Box>
-      <Box>
-        <Typography>Gender</Typography>
-      </Box>
-      <Box>
-        <Typography>Romantic Preference</Typography>
-      </Box>
-      <Box>
-        <Typography>Sexual Preference</Typography>
-      </Box>
-      <Box>
-        <Typography>Monopoly Relationship</Typography>
-      </Box>
-      <Box>
-        <Typography>Relationship Preference</Typography>
-      </Box>
-      <Box>
-        <Typography>BDSM</Typography>
-      </Box>
-      <Box>
-        <Typography>Power Exchange</Typography>
+      <Typography variant='h3'>Select Profile</Typography>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column'
+        }}
+      >
+        <Link
+          component='button'
+          href='/profile/select/age'
+          underline='none'
+        >
+          Age
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/gender'
+          underline='none'
+        >
+          Gender
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/sexualPreference'
+          underline='none'
+        >
+          Sexual Preference
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/monopolyRelationship'
+          underline='none'
+        >
+          Monopoly Relationship
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/relationshipPreference'
+          underline='none'
+        >
+          Relationship Preference
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/bdsm'
+          underline='none'
+        >
+          BDSM
+        </Link>
+        <Link
+          component='button'
+          href='/profile/select/powerExchange'
+          underline='none'
+        >
+          Power Exchange
+        </Link>
+        <Link
+          component='button'
+          href='/profile/result'
+          underline='none'
+        >
+          Result
+        </Link>
       </Box>
     </>
   );
