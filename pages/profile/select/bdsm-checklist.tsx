d45ebf063f436f7e2b438a407b89886a81ac9d16
Dokyun Lim <im.koremp@gmail.com>
2023-07-27 19:31:56 +0900

    add bdsm checklist select page

---
+++ pages/profile/select/bdsm-checklist.tsx
@@ -0,0 +1,177 @@
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Typography
+} from '@mui/material';
+
+import { analArray } from 'src/types/bdsm/Anal';
+import { bondageArray } from 'src/types/bdsm/Bondage';
+import { erogenousArray } from 'src/types/bdsm/Erogenous';
+import { filmArray } from 'src/types/bdsm/Film';
+import { multiplayArray } from 'src/types/bdsm/Multyplay';
+import { nonPenetrationArray } from 'src/types/bdsm/NonPenetration';
+import { oralArray } from 'src/types/bdsm/Oral';
+import { outdoorArray } from 'src/types/bdsm/Outdoor';
+import { roleplayArray } from 'src/types/bdsm/Roleplay';
+import { sexualPositionArray } from 'src/types/bdsm/SexualPosition';
+import { spankingArray } from 'src/types/bdsm/Spanking';
+import { toolArray } from 'src/types/bdsm/Tool';
+import { waxArray } from 'src/types/bdsm/Wax';
+import { worshipArray } from 'src/types/bdsm/Worship';
+import { dirtyArray } from 'src/types/bdsm/sadism/Dirty';
+import { mentalArray } from 'src/types/bdsm/sadism/Mental';
+import { physicalArray } from 'src/types/bdsm/sadism/Physical';
+import { softArray } from 'src/types/bdsm/sadism/Soft';
+
+export default function bdsmChecklist() {
+  return (
+    <>
+      <Typography>BDSM Checklist</Typography>
+      <FormControl>
+        <Typography>Erogenous</Typography>
+        {
+          erogenousArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Worship</Typography>
+        {
+          worshipArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Soft</Typography>
+        {
+          softArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Mental</Typography>
+        {
+          mentalArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Physical</Typography>
+        {
+          physicalArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Dirty Play</Typography>
+        {
+          dirtyArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Tool</Typography>
+        {
+          toolArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Bondage</Typography>
+        {
+          bondageArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Wax Play</Typography>
+        {
+          waxArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Spanking</Typography>
+        {
+          spankingArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Roleplay</Typography>
+        {
+          roleplayArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Flim</Typography>
+        {
+          filmArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Outdoor</Typography>
+        {
+          outdoorArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Multiplay</Typography>
+        {
+          multiplayArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Non Penetration Sexual Behavior</Typography>
+        {
+          nonPenetrationArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Sexual Position</Typography>
+        {
+          sexualPositionArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Oral</Typography>
+        {
+          oralArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+      <FormControl>
+        <Typography>Anal</Typography>
+        {
+          analArray.map((item) => (
+            <FormControlLabel control={<Checkbox />} label={item} />
+          ))
+        }
+      </FormControl>
+    </>
+  );
+};
