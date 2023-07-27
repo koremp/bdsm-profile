310be541ef216aced63cb5542599bbe475f76281
Dokyun Lim <im.koremp@gmail.com>
2023-07-27 21:46:18 +0900

    add links to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import {
 } from '@mui/material'
 
 import Head from 'next/head'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,15 @@ export default function Home() {
       </Head>
       <Box>
         <Typography variant='h3'>BDSM Profile</Typography>
+        <Box>
+          Sign In, Sign Out
+          <Link href='/profile/select'>
+            Profile Select
+          </Link>
+          <Link href='/guide'>
+            Guide
+          </Link>
+        </Box>
       </Box>
     </>
   )
