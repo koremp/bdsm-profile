d67fc4f9aeb1f8e8e970ccb049b96d1854a8bb17
Dokyun Lim <im.koremp@gmail.com>
2023-08-01 10:57:52 +0900

    add link button component

---
+++ src/components/LinkButton.tsx
@@ -0,0 +1,17 @@
+import { Button } from '@mui/material';
+import Link from 'next/link';
+
+export interface LinkButtonProps {
+  href: string
+  name: string
+};
+
+export default function LinkButton({ href, name }: LinkButtonProps) {
+  return (
+    <Button>
+      <Link href={href}>
+        {name}
+      </Link>
+    </Button>
+  );
+};
