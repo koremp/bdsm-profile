d7bbe85fdf6defb25363876b2de537366de28292
Dokyun Lim <im.koremp@gmail.com>
2023-08-01 11:55:53 +0900

    add title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material';
+
+export interface TitleProps {
+  text: string
+};
+
+export default function Title({ text }: TitleProps) {
+  return (
+    <Typography
+      variant='h3'
+      sx={{
+        textAlign: 'center',
+      }}>
+      {text}
+    </Typography>
+  );
+};
