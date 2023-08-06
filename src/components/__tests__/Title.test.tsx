c0eaed6eca69484afe3a837df399dfe36edee55c
Dokyun Lim <im.koremp@gmail.com>
2023-08-06 19:59:17 +0900

    add title test

---
+++ src/components/__tests__/Title.test.tsx
@@ -0,0 +1,12 @@
+import { render } from '@testing-library/react';
+
+import Title from 'src/components/Title';
+
+describe('Title', () => {
+  it('renders text', () => {
+    const text = 'asdfasdfasdfasdf';
+    const { getByText } = render(<Title text={text} />);
+
+    expect(getByText(text)).not.toBeNull();
+  })
+});
