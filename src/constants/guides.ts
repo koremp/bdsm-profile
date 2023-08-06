03a9091d222014606dcfe674834f41af9e319627
Dokyun Lim <im.koremp@gmail.com>
2023-08-06 18:28:16 +0900

    add guides text constants

---
+++ src/constants/guides.ts
@@ -0,0 +1,49 @@
+export const ssc: string = `​Safe, Sane and Consensual.
+
+These are some of the bedrock principals of bdsm. Someone not engaged in the BDSM lifestyle may look at BDSM and wonder how "safe, sane, and consensual" enters into the picture as they may see BDSM as unsafe or unhealthy according to misconceptions about the lifestyle
+
+First what is safe?
+
+The common dictionary states that safe is to be "secure from liability to harm, injury, danger, or risk". Safety in BDSM is taking care of their partner so that no matter how intense the scene may be, no injury or transfer of danger disease occurs; that all precautions have been taken to minimize potential dangers. This means doing the research and being knowledgeable about what your engaging in to protect your partner as well as yourself. In order to be safe you must know the risks of BDSM and which activities add spice without causing permanent or lasting damage to the emotional, mental, or physical state of either player. Before engaging in any BDSM, there should alway be a discussion of everything from roles of each person to limits, both hard and soft. From medical issues to safety measures, and all precautions should be set in place if needed.
+
+Remember Nobody wants an embarrassing visit to the emergency room or to have to call the fire department.
+
+
+Sane
+
+this means that people involved act responsibly and exercise good judgement. The ability to engage in appropriate self control is a big part of the "sane" portion of this philosophy. If you cannot control yourself, you should not enter into a situation where power exchange is a key aspect. An important part of this piece is that BDSM should not be used as a solution for serious psychological conditions or mental health concerns.
+
+Consent
+
+Consent occurs when everyone involved voluntarily agrees to the proposal or desires of another. Consent means a person is free of any tyoe of impared judgement being from alcohol, drugs, mental defect, or other types of imparement which would invalidate the consent. The point of BDSM is that there is some exchange of power, and if it is not agreed upon, it is not BDSM. If there is no consent, it is abuse in some form.`;
+
+export const cnc: string = `CNC
+In the BDSM community, CNC stands for consensual non-consent, also known as rape play. Although it varies scene to scene, it's usually an extreme power exchange where, according to previous negotiation, there is a victim overcome by a predator by force. People who enjoy this play often enjoy either the extreme lack of control or extreme control on either side of the exchange.`;
+
+export const prick: string = `Personal Responsibility, Informed, Consensual Kink or PRICK
+
+You’re not a prick if you follow PRICK. (‘Cause I’m sure no one has ever heard that joke before, amirite?) PRICK is the next evolution beyond RACK. It stands for Personal Responsibility, Informed, Consensual Kink and was developed as a response to make it clear that all kinksters should take personal responsibility for their kink. Informed means (or implies) that you understand what is about to happen — risks and all. Consensual kink is what we’re all looking for. The idea being that if you take personal responsibility for yourself and you’re informed, now you can have some consensual kink.`;
+
+export const rack: string = `Risk-Aware Consensual Kink or RACK
+
+RACK or Risk-Aware Consensual Kink was created as a response to those who felt that SSC was too vague and confusing. Who’s version of “safe” or “sane” are we talking about? What if I think something is insane but you think it’s perfectly rational? Who wins that argument?
+
+Enter RACK which lays things out more specifically.
+
+Risk-aware: Simply put you’re “aware of the risk” but I’d say it should go further than that. You should be able to name said risks and know how to prevent them.
+
+Consensual: Everyone is on board with what you’re about to do or what you’re currently doing.
+
+Kink: The thing, activity, power exchange, scene, or moment — your kinky fuckery
+
+If you’re new to RACK but you like it, use this like a checklist. Can you name the risks? Are you both able to affirm your consent to each other? Do you understand exactly what event/activity is about to take place?`;
+
+export const cccc: string = `CCCC CARING, COMMUNICATIONS, CONSENT, CAUTION
+
+1. Caring is first because if you care for someone, genuinely care for someone, then you will look out for their best interest. You want to learn about their history, hobbies, interests, dreams, aspirations and their quirks. You listen and pay attention to their needs, injuries, limits and conditions in a scene. You look at that person with care and love, which is something the other person can feel. Care builds trust between two playmates or partners and that trust is the foundation to a scene and to a relationship.
+
+2. Consent. BDSM is not your everyday activities and most often it involves two or more people. Hence it is important that everyone involved agrees to be part of it. Some resistance, edge or 'consensual noncensent' plays may require actual consent forms signed before a scene. Regardless, there must be verbal or written consent before playing out a scene.
+
+3. Communication is fundamental to BDSM. It involves negotiating a scene between playmates, learning about one another, relationship building, receiving feedbacks during and after a scene and etc. Being a good communicator is integral to a stable relationship, whether vanilla or BDSM. Lack of communication may break a vanilla relationship; lack of communication in a BDSM scene or relationship can cause severe injuries. There is no such thing as over-communicated, so talk it out.
+
+4.Caution : be aware of the risk, of the possibility of a danger and act with caution.`;
