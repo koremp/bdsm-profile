b05ee37a95ed1f5ec315a936cded38fafe3322e1
Dokyun Lim <im.koremp@gmail.com>
2023-08-09 17:31:31 +0900

    add jest override to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,20 @@ module.exports = {
     "plugin:@typescript-eslint/recommended"
   ],
   "overrides": [
+    {
+      "files": [
+        "**/__tests__/**/*.[jt]s?(x)"
+      ],
+      "env": { "jest/globals": true },
+      "globals": {
+        "context": "readonly"
+      },
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": {
+        "jest/expect-expect": "off"
+      }
+    },
   ],
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
