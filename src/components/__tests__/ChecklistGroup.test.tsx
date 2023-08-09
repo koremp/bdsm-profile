d7a2b2ceed0f45b1b35a6e80b5b676b36b4b4bd1
Dokyun Lim <im.koremp@gmail.com>
2023-08-09 19:57:00 +0900

    add checklist group test

---
+++ src/components/__tests__/ChecklistGroup.test.tsx
@@ -0,0 +1,27 @@
+import { render } from '@testing-library/react';
+
+import ChecklistGroup from 'src/components/ChecklistGroup';
+
+import { ChecklistValue } from 'src/features/bdsmChecklistSlice';
+import { erogenousArray } from 'src/types/bdsm/Erogenous';
+
+const value: ChecklistValue = {
+  me: 0,
+  you: 0,
+};
+
+test('ChecklistGroup ', () => {
+  const groupName: string = 'groupname';
+  const typeArray = erogenousArray;
+  const checklistArray = erogenousArray.map(_ => value);
+  const handleChange = jest.fn();
+
+  render(
+    <ChecklistGroup
+      groupName={groupName}
+      typeArray={typeArray}
+      checklistArray={checklistArray}
+      handleChange={handleChange}
+    />
+  );
+});
