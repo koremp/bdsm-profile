d5247d00f957a06aa07f821d0438f1d5190d84ee
Dokyun Lim <im.koremp@gmail.com>
2023-08-09 19:56:53 +0900

    add bottom test

---
+++ src/components/__tests__/Bottom.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+
+import Bottom from 'src/components/Bottom';
+
+test('Bottom', () => {
+  render(
+    <Bottom
+      backHref='backHref'
+      backName='backName'
+      nextHref='nextHref'
+      nextName='nextName'
+    />);
+});
