e3a42d1b7209a62aea31716639660e257b65259c
Dokyun Lim <im.koremp@gmail.com>
2023-08-09 16:39:46 +0900

    add LinkButton component test

---
+++ src/components/__tests__/LinkButton.tsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+import LinkButton from 'src/components/LinkButton';
+
+describe('LinkButton ', () => {
+  it('renders name.', () => {
+    const href: string = 'LinkButton Href';
+    const name: string = 'LinkButton Name';
+
+    const { getByText } = render(<LinkButton href={href} name={name} />);
+
+    expect(getByText(name)).not.toBeNull();
+  });
+
+  it('href has href value.', () => {
+    const href: string = 'LinkButton Href';
+    const name: string = 'LinkButton Name';
+
+    const { getByRole } = render(<LinkButton href={href} name={name} />);
+
+    expect(getByRole('link', { name: name })).toHaveAttribute('href', href);
+  });
+});
