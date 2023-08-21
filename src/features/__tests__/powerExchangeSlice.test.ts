4431d0e9ba8be997c26f8c189dbd5d872a5c6aea
Dokyun Lim <im.koremp@gmail.com>
2023-08-21 17:06:29 +0900

    add power exchange slice test

---
+++ src/features/__tests__/powerExchangeSlice.test.ts
@@ -0,0 +1,44 @@
+import reducer, {
+  initialState,
+  toggleExistPowerExchange,
+  PowerExchangeState,
+  setPowerExchange,
+  setOtherPowerExchange,
+  setDescription,
+} from 'src/features/powerExchangeSlice';
+import { PowerExchange } from 'src/types/PowerExchange';
+
+describe('powerExchangeSlice', () => {
+  describe('when previous state is undefined, ', () => {
+    it('returns initialState.', () => {
+      expect(reducer(undefined, { type: 'action' })).toEqual({ ...initialState });
+    });
+  });
+
+  describe('toggleExistPowerExchange', () => {
+    it('toggles exist.', () => {
+      expect(reducer({ ...initialState }, toggleExistPowerExchange()).exist).toEqual(!{ ...initialState }.exist);
+    });
+  });
+
+  describe('setPowerExchange', () => {
+    it('sets value.', () => {
+      const value: PowerExchange = PowerExchange.Other;
+      expect(reducer({ ...initialState }, setPowerExchange(value)).value).toEqual(value);
+    });
+  });
+
+  describe('setOtherPowerExchange', () => {
+    it('sets other.', () => {
+      const other: string = 'asdf';
+      expect(reducer({ ...initialState }, setOtherPowerExchange(other)).other).toEqual(other)
+    });
+  });
+
+  describe('setDescription', () => {
+    it('sets description.', () => {
+      const desc: string = 'asdf';
+      expect(reducer({ ...initialState }, setDescription(desc)).desc).toEqual(desc);
+    });
+  });
+});
