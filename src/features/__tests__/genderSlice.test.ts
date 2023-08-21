ca136f930784b366c28700131aca5e5c361dabe1
Dokyun Lim <im.koremp@gmail.com>
2023-08-21 16:46:07 +0900

    add gender slice test

---
+++ src/features/__tests__/genderSlice.test.ts
@@ -0,0 +1,44 @@
+import reducer, {
+  GenderState,
+  initialState,
+  toggleExistGender,
+  setGender,
+  setOtherGender,
+  setDescription,
+} from 'src/features/genderSlice';
+
+import { Gender } from 'src/types/Gender';
+
+describe('genderSlice', () => {
+  describe('when previous state is undefined, ', () => {
+    it('returns initialState.', () => {
+      expect(reducer({ ...initialState }, { type: 'action' })).toEqual(initialState);
+    });
+  });
+
+  describe('toggleExistGender', () => {
+    it('toggles exist.', () => {
+      expect(reducer({ ...initialState }, toggleExistGender()).exist).toEqual(!initialState.exist);
+    });
+  });
+
+  describe('setGender', () => {
+    it('sets value.', () => {
+      expect(reducer({ ...initialState }, setGender(Gender.Female)).value).toEqual(initialState.value);
+    });
+  });
+
+  describe('setOtherGender', () => {
+    it('sets other.', () => {
+      const other: string = 'asdf';
+      expect(reducer({ ...initialState }, setOtherGender(other)).other).toEqual(other);
+    });
+  });
+
+  describe('setDescription', () => {
+    it('sets description.', () => {
+      const desc: string = 'asdf';
+      expect(reducer({ ...initialState }, setDescription(desc)).desc).toEqual(desc);
+    });
+  });
+});
