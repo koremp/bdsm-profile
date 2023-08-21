72445a66690ae5805bde16f8cf06383447fcb27b
Dokyun Lim <im.koremp@gmail.com>
2023-08-21 16:38:14 +0900

    add bdsm slice test

---
+++ src/features/__tests__/bdsmSlice.test.ts
@@ -0,0 +1,50 @@
+import reducer, {
+  BDSMState,
+  toggleExistBDSM,
+  setBDSMs,
+  setOtherBDSMs,
+  setDescription,
+} from 'src/features/bdsmSlice';
+
+import { BDSM } from 'src/types/BDSM';
+
+const defaultState: BDSMState = {
+  exist: false,
+  values: [],
+  others: [],
+  desc: '',
+};
+
+describe('bdsmSlice', () => {
+  describe('when previous state is undefined, ', () => {
+    it('returns initialState.', () => {
+      expect(reducer(undefined, { type: 'action' })).toEqual({ ...defaultState });
+    });
+  });
+
+  describe('toggleExistBDSM', () => {
+    it('toggles exist.', () => {
+      expect(reducer({ ...defaultState }, toggleExistBDSM()).exist).toEqual(!defaultState.exist);
+    });
+  });
+
+  describe('setBDSMs', () => {
+    it('sets values.', () => {
+      expect(reducer({ ...defaultState }, setBDSMs(BDSM[0])).values[0]).toEqual(BDSM[0]);
+    });
+  });
+
+  describe('setOtherBDSMs', () => {
+    it('set OtherBDSMs.', () => {
+      const other: string = 'asdf';
+      expect(reducer({ ...defaultState }, setOtherBDSMs(other)).others[0]).toEqual(other);
+    });
+  });
+
+  describe('setDescription', () => {
+    it('set description.', () => {
+      const desc: string = 'asdf';
+      expect(reducer({ ...defaultState }, setDescription(desc)).desc).toEqual(desc);
+    })
+  })
+});
