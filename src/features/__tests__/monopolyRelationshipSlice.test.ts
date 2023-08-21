42a40b54cfed906e2f9895e8ea53d6e1f8b0032f
Dokyun Lim <im.koremp@gmail.com>
2023-08-21 16:59:46 +0900

    add monopoly relationship slice test

---
+++ src/features/__tests__/monopolyRelationshipSlice.test.ts
@@ -0,0 +1,44 @@
+import reducer, {
+  initialState,
+  toggleExistMonopolyRelationship,
+  MonopolyRelationshipState,
+  setMonopolyRelationship,
+  setOtherMonopolyRelationship,
+  setDescription,
+} from 'src/features/monopolyRelationshipSlice';
+import { MonopolyRelationship } from 'src/types/MonopolyRelationship';
+
+describe('monopolyRelationshipSlice', () => {
+  describe('when previous state is undefined, ', () => {
+    it('returns initialState.', () => {
+      expect(reducer(undefined, { type: 'action' })).toEqual({ ...initialState });
+    });
+  });
+
+  describe('toggleExistGender', () => {
+    it('toggles exist.', () => {
+      expect(reducer({ ...initialState }, toggleExistMonopolyRelationship()).exist).toEqual(!{ ...initialState }.exist);
+    });
+  });
+
+  describe('setMonopolyRelationship', () => {
+    it('sets value.', () => {
+      const value: MonopolyRelationship = MonopolyRelationship.Monogamy;
+      expect(reducer({ ...initialState }, setMonopolyRelationship(value)).value).toEqual(value);
+    });
+  });
+
+  describe('setOtherMonopolyRelationship', () => {
+    it('sets other.', () => {
+      const other: string = 'asdf';
+      expect(reducer({ ...initialState }, setOtherMonopolyRelationship(other)).other).toEqual(other)
+    });
+  });
+
+  describe('setDescription', () => {
+    it('sets description.', () => {
+      const desc: string = 'asdf';
+      expect(reducer({ ...initialState }, setDescription(desc)).desc).toEqual(desc);
+    });
+  });
+});
