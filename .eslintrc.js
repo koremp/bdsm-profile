78f60bb8d2e089c2cd8b7a4f4873a22de05cdd3a
Clair <fried.pride.fry@gmail.com>
2023-08-23 16:09:44 +0900

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,7 @@
 module.exports = {
   "extends": [
     "next/core-web-vitals",
-    "airbnb",
-    "airbnb-typescript"
+    "eslint:recommended",
+    "next"
   ],
-  "plugins": [
-    "testing-library",
-    "@typescript-eslint"
-  ],
-  "parser": "@typescript-eslint/parser",
-  "overrides": [
-    {
-      "files": [
-        "**/__tests__/**/*.[jt]s?(x)"
-      ],
-      "env": { "jest/globals": true },
-      "globals": {
-        "context": "readonly"
-      },
-      "plugins": ["jest"],
-      "extends": ["plugin:jest/recommended"],
-      "rules": {
-        "jest/expect-expect": "off"
-      }
-    },
-  ],
-  "parserOptions": {
-    "project": "tsconfig.json"
-  },
-  "rules": {
-    "react/react-in-jsx-scope": "off",
-    "react/jsx-props-no-spreading": "off",
-    "react/jsx-filename-extension": [1, { "extensions": [".tsx", ".jsx"] }],
-    "import/extensions": [
-      "error",
-      "ignorePackages",
-      {
-        "js": "never",
-        "jsx": "never",
-        "ts": "never",
-        "tsx": "never"
-      }
-    ],
-    "react/require-default-props": "off",
-    "@typescript-eslint/naming-convention": "off"
-  },
 };
