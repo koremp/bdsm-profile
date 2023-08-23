acd16b23c2d18b4da05cbf24de5868e580dce96d
Clair <fried.pride.fry@gmail.com>
2023-08-23 16:13:55 +0900

    add jest plugin to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,4 +4,23 @@ module.exports = {
     "eslint:recommended",
     "next"
   ],
+  "overrides": [
+    {
+      "files": [
+        "**/__tests__/**/*.[jt]s?(x)"
+      ],
+      "env": { "jest/globals": true },
+      "globals": {
+        "context": "readonly"
+      },
+      "plugins": ["jest"],
+      "extends": ["plugin:jest/recommended"],
+      "rules": {
+        "jest/expect-expect": "off"
+      }
+    },
+  ],
+  "parserOptions": {
+    "project": "./tsconfig.json"
+  },
 };
