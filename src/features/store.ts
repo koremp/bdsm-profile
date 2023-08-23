a4283d19dcc30e906cdc97685d7c4acb2391ce55
Dokyun Lim <im.koremp@gmail.com>
2023-08-23 15:53:11 +0900

    add default export of store

--- src/features/store.ts
+++ src/features/store.ts
@@ -33,6 +33,9 @@ export function setupStore(preloadedState?: PreloadedState<RootState>) {
   });
 };
 
+const store = setupStore();
+export default store;
+
 export type RootState = ReturnType<typeof rootReducer>;
 export type AppStore = ReturnType<typeof setupStore>;
 export type AppDispatch = AppStore['dispatch'];
