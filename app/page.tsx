f3126b30a44fb14f11db14abffedaede9e34c1cf
Dokyun Lim <im.koremp@gmail.com>
2023-08-26 13:48:47 +0900

    remove head of page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Head from 'next/head';
-
 import { ButtonGroup } from '@mui/material';
 import { deepPurple } from '@mui/material/colors';
 
@@ -8,26 +6,19 @@ import Page from 'src/components/Page';
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>BDSM Profile</title>
-        <meta name="description" content="Index Page of BDSM Profile" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Page
-        titleText="BDSM Profile"
+    <Page
+      titleText="BDSM Profile"
+    >
+      {/* todo: add signin button */}
+      <ButtonGroup
+        sx={{ backgroundColor: `${deepPurple[100]}` }}
+        fullWidth
+        orientation="vertical"
+        aria-label="bdsm profile button group"
       >
-        {/* todo: add signin button */}
-        <ButtonGroup
-          sx={{ backgroundColor: `${deepPurple[100]}` }}
-          fullWidth
-          orientation="vertical"
-          aria-label="bdsm profile button group"
-        >
-          <LinkButton href="/profile/select" name="Select Profile" />
-          <LinkButton href="/guide" name="Guide" />
-        </ButtonGroup>
-      </Page>
-    </>
+        <LinkButton href="/profile/select" name="Select Profile" />
+        <LinkButton href="/guide" name="Guide" />
+      </ButtonGroup>
+    </Page>
   );
 }
