b753eb03856483e467e8e39946e50de48536dedb
Dokyun Lim <im.koremp@gmail.com>
2023-08-26 13:49:17 +0900

    add head to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
 }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <title>BDSM Profile</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body>
         <Providers>
           <Container
