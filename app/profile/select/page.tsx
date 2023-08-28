03224aa7ad9da49029c91b065afd9a2912933994
Dokyun Lim <im.koremp@gmail.com>
2023-08-28 16:32:10 +0900

    fix href of select page

--- app/profile/select/page.tsx
+++ app/profile/select/page.tsx
@@ -8,11 +8,11 @@ export default function IndexSelectProfilePage() {
   const links: LinkButtonProps[] = [
     { href: '/profile/select/age', name: 'Age' },
     { href: '/profile/select/gender', name: 'Gender' },
-    { href: '/profile/select/romanticPreference', name: 'Romantic Preference' },
-    { href: '/profile/select/sexualPreference', name: 'Sexual Preference' },
-    { href: '/profile/select/monopolyRelationship', name: 'Monopoly Relationship' },
+    { href: '/profile/select/romantic-preference', name: 'Romantic Preference' },
+    { href: '/profile/select/sexual-preference', name: 'Sexual Preference' },
+    { href: '/profile/select/monopoly-relationship', name: 'Monopoly Relationship' },
     { href: '/profile/select/bdsm', name: 'BDSM' },
-    { href: '/profile/select/powerExchange', name: 'Power Exchange' },
+    { href: '/profile/select/power-exchange', name: 'Power Exchange' },
     { href: '/profile/select/bdsm-checklist', name: 'BDSM Checklist' },
   ];
 
@@ -22,12 +22,13 @@ export default function IndexSelectProfilePage() {
         fullWidth
         orientation="vertical"
         aria-label="profile select button group"
+        key="profile-select-button-group"
         sx={{
           backgroundColor: `${deepPurple[100]}`,
         }}
       >
         {links.map(({ href, name }) => (
-          <LinkButton href={href} name={name} />
+          <LinkButton key={href} href={href} name={name} />
         ))}
       </ButtonGroup>
       <ButtonGroup
@@ -37,6 +38,7 @@ export default function IndexSelectProfilePage() {
           backgroundColor: `${deepPurple[100]}`,
         }}
         aria-label="bottom button group"
+        key="bottom-button-group"
       >
         <LinkButton href="/" name="back" />
       </ButtonGroup>
