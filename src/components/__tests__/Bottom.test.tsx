34a162c60c5425ce1fa578be0f5038911726f460
Dokyun Lim <im.koremp@gmail.com>
2023-09-04 14:18:42 +0900

    update bottom component test

--- src/components/__tests__/Bottom.test.tsx
+++ src/components/__tests__/Bottom.test.tsx
@@ -1,14 +1,69 @@
 import { render } from '@testing-library/react';
 
-import Bottom from 'src/components/Bottom';
+import Bottom, { ButtonProps } from 'src/components/Bottom';
 
-test('Bottom', () => {
-  render(
-    <Bottom
-      backHref="backHref"
-      backName="backName"
-      nextHref="nextHref"
-      nextName="nextName"
-    />,
-  );
+describe('Bottom', () => {
+  describe('when middle is undefined,', () => {
+    it('renders back and next button.', () => {
+      const back: ButtonProps = {
+        href: 'back-href',
+        name: 'back-name',
+      };
+
+      const next: ButtonProps = {
+        href: 'next-href',
+        name: 'next-name',
+      };
+
+      const { getByText } = render(
+        <Bottom
+          back={back}
+          middle={undefined}
+          next={next}
+        />,
+      );
+
+      expect(getByText(back.name)).not.toBeNull();
+      expect(getByText(next.name)).not.toBeNull();
+    });
+  });
+
+  describe('when middle is not undefined,', () => {
+    it('renders back, middle, next buton.', () => {
+      const back: ButtonProps = {
+        href: 'back-href',
+        name: 'back-name',
+      };
+
+      const middle: ButtonProps = {
+        href: 'middle-href',
+        name: 'middle-name',
+      };
+
+      const next: ButtonProps = {
+        href: 'next-href',
+        name: 'next-name',
+      };
+      const { getByText } = render(
+        <Bottom
+          back={{
+            href: 'back-href',
+            name: 'back-text',
+          }}
+          middle={{
+            href: 'middle-href',
+            name: 'middle-text',
+          }}
+          next={{
+            href: 'next-href',
+            name: 'next-text',
+          }}
+        />,
+      );
+
+      expect(getByText(back.name)).not.toBeNull();
+      expect(getByText(middle.name)).not.toBeNull();
+      expect(getByText(next.name)).not.toBeNull();
+    });
+  });
 });
